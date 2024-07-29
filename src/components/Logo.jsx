a7a0a8ce1d6da17fab36943a776d0688ff3242c1
adarsh973 <adarshsrivastava973@gmail.com>
2024-07-29 15:32:01 +0530

    added logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Logo({width = '100px'}) {
   return (
-    <div>Logo</div>
+    <div>
+      <img src='https://lh3.googleusercontent.com/gg/AJIvXisXQbqqP1JcBiq0rMp2WsoYxGsOBXRX35qQIqbhPg4n20gJ7Mp3RQHbAfwmSD2FvPGQISYt7DH_um93yEv_1x7hzG7KoYrHE9qowZhwMyJbOQqr39rPk8Cs3x5dQ8xYfNUtTFEFigQCXQJ6H7RPNLAw04vtwjDHd1ekqLdGwW5Ob4r2TiTf'
+      alt='logo' className='h-15 w-20'/>
+    </div>
   )
 }
 
