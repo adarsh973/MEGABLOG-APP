378d71ebebbc7d1e221cf684bc42bf62bf21decb
adarsh973 <adarshsrivastava973@gmail.com>
2024-07-29 16:04:46 +0530

    image change

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width = '100px'}) {
   return (
     <div>
-      <img src='https://lh3.googleusercontent.com/gg/AJIvXisXQbqqP1JcBiq0rMp2WsoYxGsOBXRX35qQIqbhPg4n20gJ7Mp3RQHbAfwmSD2FvPGQISYt7DH_um93yEv_1x7hzG7KoYrHE9qowZhwMyJbOQqr39rPk8Cs3x5dQ8xYfNUtTFEFigQCXQJ6H7RPNLAw04vtwjDHd1ekqLdGwW5Ob4r2TiTf'
+      <img src='https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572-768x591.png'
       alt='logo' className='h-15 w-20'/>
     </div>
   )
